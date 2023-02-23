eda14eb6b590d3d04c5e7069e7d2832b905f6eae
alexandernair <alexnair@me.com>
2023-02-22 18:29:18 -0600

    getting nmap to work

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
 from flask import Flask
-
+import psycopg2
+import nmap
+import socket
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
 
+
 @app.route('/alex')
 def alex():
     return 'Hello, Alex!'
 
-@app.route('/nmap/<ip>')
-def nmap(ip):
+
+@app.route('/nmap')
+def deviceInfo():
+    hostname = socket.gethostname()
+    IPAddr = socket.gethostbyname(hostname)
     # TODO: run nmap and return the results
     devices = []
     # populate devices
-    
-    return {'ip': ip, 'devices': devices}
+    print("Ip address for ", hostname, "is", IPAddr)
+    nm = nmap.PortScanner()
+    devices = nm.scan(hosts=IPAddr + '/24',
+                      arguments="-sV -O -T4 -n -Pn -p 80,443 --max-rtt-timeout 100ms")
+    print(devices)
+    return {'devices': devices}
